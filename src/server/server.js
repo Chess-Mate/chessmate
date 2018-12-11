6b58705d3ea40b7fd5863c410ed2ca3713e074bf
Nian Liu <nianliu@Nians-MacBook-Pro.local>
2018-12-11 14:40:53 -0500

    push setup

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const os = require('os');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.set({'Content-Type': 'application/json'})
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
