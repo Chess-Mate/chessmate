c9499973127396be4f1ab120a585913802ca5929
Nian Liu <nianliu@Nians-MacBook-Pro.local>
2018-12-13 18:35:38 -0500

    changes

--- src/client/controllers/pieces/pawn.js
+++ src/client/controllers/pieces/pawn.js
@@ -15,7 +15,7 @@ class Pawn extends Piece {
     // can move forward 1 space
     let i = parseInt(currentRow) + 1;
     if (i > 0 && i < 9) {
-      if (piecesObj[currentColumn + i]) {
+      if (piecesObj[currentColumn + i] && piecesObj[currentColumn + i].color === 'white') {
         if (this.color !== piecesObj[currentColumn + i].color) {
           availableMoves.push(currentColumn + i)
         }
@@ -24,8 +24,19 @@ class Pawn extends Piece {
       }
     }
 
-    // can move forward 2 spaces if on home row
-    if (parseInt(currentRow) === 2 || parseInt(currentRow) === 7) {
+    let i = parseInt(currentRow) - 1;
+    if (i > 0 && i < 9) {
+      if (piecesObj[currentColumn + i] && piecesObj[currentColumn + i].color === 'black') {
+        if (this.color !== piecesObj[currentColumn + i].color) {
+          availableMoves.push(currentColumn + i)
+        }
+      } else {
+        availableMoves.push(currentColumn + i)
+      }
+    }
+
+    // can move forward 2 spaces if on home row, for white
+    if (parseInt(currentRow) === 2 && piecesObj[currentColumn + i].color === 'white') {
       i = parseInt(currentRow) + 2;
       if (i > 0 && i < 9) {
         if (piecesObj[currentColumn + i]) {
@@ -38,9 +49,23 @@ class Pawn extends Piece {
       }
     }
 
-    // can move diagonally right 1 space if occupied by opponent's color
+      //for black
+    if (parseInt(currentRow) === 7 && piecesObj[currentColumn + i].color === 'black') {
+      i = parseInt(currentRow) - 2;
+      if (i > 0 && i < 9) {
+        if (piecesObj[currentColumn + i]) {
+          if (this.color !== piecesObj[currentColumn + i].color) {
+            availableMoves.push(currentColumn + i)
+          }
+        } else {
+          availableMoves.push(currentColumn + i)
+        }
+      }
+    }
+
+    // can move diagonally right 1 space if occupied by opponent's color / white
     i = parseInt(currentRow) + 1;
-    if (currentColumn.length === 1) {
+    if (currentColumn.length === 1 && piecesObj[currentColumn + i].color === 'white') {
 
       currentColumn = currentColumn.charCodeAt() + 1;
       if (currentColumn > 97 && currentColumn < 104 && i > 0 && i < 9) {
@@ -54,10 +79,43 @@ class Pawn extends Piece {
     }
     currentColumn = String.fromCharCode(currentColumn);
 
-    // can move diagonally left 1 space if occupied by opponent's color
+    // can move diagonally right 1 space if occupied by opponent's color / black
+    i = parseInt(currentRow) - 1;
+    if (currentColumn.length === 1 && piecesObj[currentColumn + i].color === 'black') {
+
+      currentColumn = currentColumn.charCodeAt() + 1;
+      if (currentColumn > 97 && currentColumn < 104 && i > 0 && i < 9) {
+
+        if (piecesObj[String.fromCharCode(currentColumn) + i]) {
+            if (this.color !== piecesObj[String.fromCharCode(currentColumn) + i].color) {
+              availableMoves.push(String.fromCharCode(currentColumn) + i)
+            }
+        } 
+      }
+    }
+    currentColumn = String.fromCharCode(currentColumn);
+
+    // can move diagonally left 1 space if occupied by opponent's color / white
     currentColumn = this.coordinates[0];
     i = parseInt(currentRow) + 1;
-    if (currentColumn.length === 1) {
+    if (currentColumn.length === 1 && piecesObj[currentColumn + i].color === 'white') {
+
+      currentColumn = currentColumn.charCodeAt() - 1;
+      if (currentColumn > 97 && currentColumn < 104 && i > 0 && i < 9) {
+
+        if (piecesObj[String.fromCharCode(currentColumn) + i]) {
+          if (this.color !== piecesObj[String.fromCharCode(currentColumn) + i].color) {
+            availableMoves.push(String.fromCharCode(currentColumn) + i)
+          }
+        }
+      }
+    }
+    currentColumn = String.fromCharCode(currentColumn);
+
+    // can move diagonally left 1 space if occupied by opponent's color / black
+    currentColumn = this.coordinates[0];
+    i = parseInt(currentRow) - 1;
+    if (currentColumn.length === 1 && piecesObj[currentColumn + i].color === 'black') {
 
       currentColumn = currentColumn.charCodeAt() - 1;
       if (currentColumn > 97 && currentColumn < 104 && i > 0 && i < 9) {
